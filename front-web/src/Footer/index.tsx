e109dd7c20f101dc03193e6a641bfb65122e4623
evsdomingues@gmail.com <evsdomingues@gmail.com>
2021-01-10 23:01:43 -0300

    Fix Bug Env Vars

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -9,10 +9,10 @@ function Footer() {
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.linked.com/school/devsuperior" target="_new">
+                <a href="https://www.linkedin.com/in/evandrodomings/" target="_new">
                     <LinkedinIcon/>
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig" target="_new">
+                <a href="https://www.instagram.com/esouzadom/" target="_new">
                     <InstagramIcon/>
                 </a>
             </div>
