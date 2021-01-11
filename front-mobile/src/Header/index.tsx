e10b989fe8d3cecc1542c686c052653aa0ca1db3
Evandro Domingues <evsdomingues@gmail.com>
2021-01-11 12:21:42 -0300

    Starting the Header Component

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Header from './src/Header';
-
-export default function App() {
+function Header() {
   return (
     <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />
+      <Text>DS Delivery</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+      backgroundColor: '#DA5C5C'
   },
 });
+
+export default Header;
